7848e925e31503b9e820fcf767e776dab35cac6c
Michael <bordenmike518@gmail.com>
2017-08-18 22:25:43 -0400

    Create cnc_machine.c

---
+++ src/cnc_machine.c
@@ -0,0 +1,37 @@
+#include "lib/cnc.h"
+
+#define MAX_BUF     (128)
+
+
+
+int main(void) {
+
+    uint8_t input_index = 0;
+    char input_char,
+         input_arr[MAX_BUF];
+
+    usart_init();
+    limits_init();
+    motors_init();
+    
+    set_position(0.0, 0.0);
+    
+    usart_ready();
+
+    while(1) {
+        motors_disable();
+        while(usart_available() > 0) {
+            _delay_ms(100);
+            input_char = usart_read();
+            if (input_index++ < MAX_BUF-1 && input_char != '\0')
+                input_arr[input_index] = input_char;
+        }
+        input_arr[input_index] = '\0';
+        input_index = 0;
+        motors_enable();
+        gcode_interpret(&input_arr[0]);
+        usart_ready();
+    }
+
+    return (0);
+}
